f9e11438b323594f4e368a316b212aa840e63c8d
Dauletgali Karim <dauletgalikarim@MacBook-Pro-Dauletgali.local>
2020-07-03 15:09:33 +0600

    ïnitial xcode

---
+++ gloom_project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gloom_project
+//
+//  Created by Dauletgali Karim on 7/3/20.
+//  Copyright © 2020 Dauletgali Karim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
