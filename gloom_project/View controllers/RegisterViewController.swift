af27f9c2f6940b85a2cd27fe5da53ff2002e44ff
Dauletgali Karim <dauletgalikarim@MacBook-Pro-Dauletgali.local>
2020-07-04 01:04:17 +0600

    Dauletgali login page setup firebase

---
+++ gloom_project/View controllers/RegisterViewController.swift
@@ -0,0 +1,42 @@
+//
+//  RegisterViewController.swift
+//  gloom_project
+//
+//  Created by Dauletgali Karim on 7/3/20.
+//  Copyright © 2020 Dauletgali Karim. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    
+    @IBOutlet weak var phoneNumberTextfield: UITextField!
+    
+    
+    @IBOutlet weak var otpTextfield: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+
+    @IBAction func registerButtonClick(_ sender: Any) {
+        
+        
+    }
+}
